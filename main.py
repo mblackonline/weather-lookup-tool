b6d316a98c1630040353b9c179312e33173ac47d
Matt Black <mblackonline.git@gmail.com>
2023-07-19 08:25:27 -0400

    Update main.py to integrate urlencode() function
    Based on recommendations from a Snyk code review, I integrated the urlencode() function from the urllib.parse module to encode the parameters in the URL. This should help to sanitize the code by ensuring that any special characters in the parameters are properly encoded and help reduce the threat of a Server-Side Request Forgery.

--- main.py
+++ main.py
@@ -7,26 +7,47 @@ import requests
 import time
 import os
 from dotenv import load_dotenv
+from urllib.parse import urlencode
+
 load_dotenv()
 
-api_key = ""  # Replace this value with your own API key from openweathermap.com.
+# Replace this value with your own API key from openweathermap.com.
+api_key = ""
 
-city_name_state = input('Enter a city and state: ')  # create a variable to store the city name
-zip_code = input('Enter the city zip code: ')  # create a variable to store the zip code
-country = 'US'  # input('Enter your country code:') # create a variable to store the country code
-link_state = f'https://api.openweathermap.org/data/2.5/weather?zip={zip_code},{country}&appid={os.getenv("api_key")}&units=imperial'  # create a variable to store the link to the API
-response = requests.get(link_state).json()  # create a variable to store the response from the API
+# create a variable to store the city name
+city_name_state = input('Enter a city and state: ')
+# create a variable to store the zip code
+zip_code = input('Enter the city zip code: ')
+# input('Enter your country code:') # create a variable to store the country code
+country = 'US'
+
+# create a dictionary of parameters to be passed in the URL
+payload = {'zip': f'{zip_code},{country}', 'appid': os.getenv("api_key"), 'units': 'imperial'}
+# encode the parameters in the URL using urlencode() function from urllib.parse module
+url = f'https://api.openweathermap.org/data/2.5/weather?{urlencode(payload)}'
+
+# make a request to the API using the encoded URL
+response = requests.get(url).json()
 
 # print(response)  # uncomment to see the JSON response
 
-current_temp = response['main']['temp']  # create a variable to store the current temperature
-feels_like = response['main']['feels_like']  # create a variable to store what it feels like outside
-temp_min = response['main']['temp_min']  # create a variable to store the low temperature
-temp_max = response['main']['temp_max']  # create a variable to store the high temperature
-humidity = response['main']['humidity']  # create a variable to store the humidity level
-sunrise = response['sys']['sunrise']  # create a variable to store the sunrise time
-sunset = response['sys']['sunset']  # create a variable to store the sunset time
-sunrise_time_val = time.strftime('%#I:%M %p', time.localtime(sunrise))  # The "#" is to remove the leading 0 from the time.  Time has been converted from Unix/Epoch time to standard time.
+# create a variable to store the current temperature
+current_temp = response['main']['temp']
+# create a variable to store what it feels like outside
+feels_like = response['main']['feels_like']
+# create a variable to store the low temperature
+temp_min = response['main']['temp_min']
+# create a variable to store the high temperature
+temp_max = response['main']['temp_max']
+# create a variable to store the humidity level
+humidity = response['main']['humidity']
+# create a variable to store the sunrise time
+sunrise = response['sys']['sunrise']
+# create a variable to store the sunset time
+sunset = response['sys']['sunset']
+
+# Below, the "#" is to remove the leading 0 from the time.  Time has been converted from Unix/Epoch time to standard time.
+sunrise_time_val = time.strftime('%#I:%M %p', time.localtime(sunrise))
 sunset_time_val = time.strftime('%#I:%M %p', time.localtime(sunset))
 
 print(f' - At the location: {city_name_state}, the temperature is currently {current_temp} Fahrenheit.')
